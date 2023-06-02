ca4adc594c28781844f6bc0770d6fbec27ac3b69
nicolas <nico.divirgilio@gmail.com>
2023-06-02 16:22:58 -0300

    prueba

--- app/src/main/java/com/example/calculadoravuelo/CalculoActivity.java
+++ app/src/main/java/com/example/calculadoravuelo/CalculoActivity.java
@@ -1,33 +1,82 @@
 package com.example.calculadoravuelo;
 
 
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
+import com.jakewharton.threetenabp.AndroidThreeTen;
+import org.threeten.bp.LocalTime;
 import java.util.Calendar;
+
 
 public class CalculoActivity extends AppCompatActivity {
 
-    long h1 = 0, h2 = 0, m1 = 0, m2 = 0;
-    long hora = 0, h = 0, m = 0;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+
+            if(id==R.id.menu_home){
+                Intent myIntent = new Intent(this, MainActivity.class);
+                startActivity(myIntent);
+            }
+            if(id==R.id.menu_des){
+                Intent myIntent = new Intent(this, CalculoActivity.class);
+                startActivity(myIntent);
+            }
+            if(id==R.id.menu_cab){
+            Toast.makeText(this, "¡Próximamente!", Toast.LENGTH_SHORT).show();
+            }
+            if(id==R.id.menu_int){
+            Toast.makeText(this, "¡Próximamente!", Toast.LENGTH_SHORT).show();
+            }
+            if(id==R.id.decreto){
+            String _url = "https://www.argentina.gob.ar/sites/default/files/infoleg/dto877-1.pdf";
+            Uri _link  = Uri.parse(_url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, _link);
+            startActivity(intent);
+            }
+            if(id==R.id.help){
+            Intent myIntent = new Intent(this, Help.class);
+            startActivity(myIntent);
+            }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    int h1 = 0, h2 = 0, m1 = 0, m2 = 0;
+    int hora = 0, h = 0, m = 0;
     TextView res, ha, hb, nuevoEtd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculo);
-        res=(TextView) findViewById(R.id.res);
-        ha=(TextView) findViewById(R.id.h1);
-        hb=(TextView) findViewById(R.id.h2);
-        nuevoEtd=(TextView) findViewById(R.id.nuevoEtd);
+        res = (TextView) findViewById(R.id.res);
+        ha = (TextView) findViewById(R.id.h1);
+        hb = (TextView) findViewById(R.id.h2);
+        nuevoEtd = (TextView) findViewById(R.id.nuevoEtd);
+        AndroidThreeTen.init(this);
     }
-    public void primero (View v) {
+
+
+    public void primero(View v) {
+        //Inicio del tiempo de servicio de vuelo (1 hora antes del ETD)
         final Calendar cal = Calendar.getInstance();
         final int hora = cal.get(Calendar.HOUR_OF_DAY);
         final int minuto = cal.get(Calendar.MINUTE);
@@ -46,7 +95,10 @@ public class CalculoActivity extends AppCompatActivity {
         recogerHora.show();
 
     }
-    public void segundo (View v){
+
+
+    public void segundo(View v) {
+        //Eta más 30 min
         final Calendar cal = Calendar.getInstance();
         final int hora = cal.get(Calendar.HOUR_OF_DAY);
         final int minuto = cal.get(Calendar.MINUTE);
@@ -66,100 +118,138 @@ public class CalculoActivity extends AppCompatActivity {
         recogerHora.show();
 
     }
-    public void diferencia(){
 
-        if (h1>=0 && h2 >=0) {
-            if(h1 >= h2){
-                hora = (((h2+24) * 60) + m2) - ((h1 * 60) + m1);
+
+    public void diferencia() {
+        //Tiempo de servicio de vuelo
+        if (h1 >= 0 && h2 >= 0) {
+            if (h1 >= h2) {
+                hora = (((h2 + 24) * 60) + m2) - ((h1 * 60) + m1);
             } else {
                 hora = ((h2 * 60) + m2) - ((h1 * 60) + m1);
             }
             h = hora / 60;
-            m = hora - (h*60);
+            m = hora - (h * 60);
+
             descanso();
         }
         res.setText("Tiempo servicio de vuelo: " + h + " hrs " + m + " min ");
 
 
     }
-    public void descanso() {
-        long etd = 0, hetd = 0, metd = 0;
-
-        if (h1 >= 6  && h2 >= 6) {
-            if (h == 6) {
-                etd = ((h2 + 8) * 60) + m2 + 45;
-            } else if (h == 7 || h == 8) {
-                etd = ((h2 + 10) * 60) + m2 + 45;
-            } else if (h == 9) {
-                etd = ((h2 + 11) * 60) + m2 + 45;
-            } else if (h == 10) {
-                etd = ((h2 + 12) * 60) + m2 + 45;
-            } else if (h == 11) {
-                etd = ((h2 + 13) * 60) + m2 + 45;
-            } else if (h == 12) {
-                etd = ((h2 + 14) * 60) + m2 + 45;
-            } else if (h == 13) {
-                etd = ((h2 + 15) * 60) + m2 + 45;
-            } else if (h == 14) {
-                etd = ((h2 + 16) * 60) + m2 + 45;
-            } else if (h == 15) {
-                etd = ((h2 + 17) * 60) + m2 + 45;
-            } else if (h == 16) {
-                etd = ((h2 + 18) * 60) + m2 + 45;
-            }
-        }else if (h1 <= 3 && h2 <3 || h2 >= 9 ){
-            if (h == 6) {
-                etd = ((h2 + 12) * 60) + m2 + 45;
-            } else if (h == 7 || h == 8) {
-                etd = ((h2 + 14) * 60) + m2 + 45;
-            } else if (h == 9) {
-                etd = ((h2 + 15) * 60) + m2 + 45;
-            } else if (h == 10) {
-                etd = ((h2 + 16) * 60) + m2 + 45;
-            } else if (h == 11) {
-                etd = ((h2 + 17) * 60) + m2 + 45;
-            } else if (h == 12) {
-                etd = ((h2 + 18) * 60) + m2 + 45;
-            } else if (h == 13) {
-                etd = ((h2 + 18) * 60) + m2 + 45;
-            } else if (h == 14) {
-                etd = ((h2 + 19) * 60) + m2 + 45;
-            } else if (h == 15) {
-                etd = ((h2 + 19) * 60) + m2 + 45;
-            } else if (h == 16) {
-                etd = ((h2 + 20) * 60) + m2 + 45;
-            }
-        }else {
-            if (h == 6) {
-                etd = ((h2 + 10) * 60) + m2 + 45;
-            } else if (h == 7 || h == 8) {
-                etd = ((h2 + 12) * 60) + m2 + 45;
-            } else if (h == 9) {
-                etd = ((h2 + 13) * 60) + m2 + 45;
-            } else if (h == 10) {
-                etd = ((h2 + 14) * 60) + m2 + 45;
-            } else if (h == 11) {
-                etd = ((h2 + 15) * 60) + m2 + 45;
-            } else if (h == 12) {
-                etd = ((h2 + 16) * 60) + m2 + 45;
-            } else if (h == 13) {
-                etd = ((h2 + 16) * 60) + m2 + 45;
-            } else if (h == 14) {
-                etd = ((h2 + 17) * 60) + m2 + 45;
-            } else if (h == 15) {
-                etd = ((h2 + 17) * 60) + m2 + 45;
-            } else if (h == 16) {
-                etd = ((h2 + 18) * 60) + m2 + 45;
-            }
 
 
-                }
-        hetd = etd  / 60;
-        metd = etd  - (hetd*60);
-        nuevoEtd.setText("Próximo ETD: " + hetd + "hrs" + metd + "min");
-            }
+     public void descanso() {
+         LocalTime eta = LocalTime.of(h2, m2);
 
-        }
+         LocalTime netd = LocalTime.ofSecondOfDay(0);
+         //Sin tocar el nocturno
+         if (h1 >= 6 && h2 >= 10) {
+             if (hora <= 360) {
+
+                 netd = eta.plusHours(8).plusMinutes(45);
+             } else if (hora <= 480) {
+
+                 netd = eta.plusHours(10).plusMinutes(45);
+             } else if (hora <= 540) {
+
+                 netd = eta.plusHours(11).plusMinutes(45);
+             } else if (hora <= 600) {
+
+                 netd = eta.plusHours(12).plusMinutes(45);
+             } else if (hora <= 660) {
+
+                 netd = eta.plusHours(13).plusMinutes(45);
+             } else if (hora <= 720) {
+
+                 netd = eta.plusHours(14).plusMinutes(45);
+             } else if (hora <= 780) {
+
+                 netd = eta.plusHours(15).plusMinutes(45);
+             } else if (hora <= 840) {
+
+                 netd = eta.plusHours(16).plusMinutes(45);
+             } else if (hora <= 900) {
+
+                 netd = eta.plusHours(17).plusMinutes(45);
+             } else if (hora <= 960) {
+
+                 netd = eta.plusHours(18).plusMinutes(45);
+             }
+             //Tocando nocturno
+         } else if (h1 >= 3 && h2 < 3 || h2 >= 9) {
+             if (hora <= 360) {
+
+                 netd = eta.plusHours(10).plusMinutes(45);
+             } else if (hora <= 480) {
+
+                 netd =eta.plusHours(12).plusMinutes(45);
+             } else if (hora <= 540) {
+
+                 netd = eta.plusHours(13).plusMinutes(45);
+             } else if (hora <= 600) {
+
+                 netd =eta.plusHours(14).plusMinutes(45);
+             } else if (hora <= 660) {
+
+                 netd =eta.plusHours(15).plusMinutes(45);
+             } else if (hora <= 720) {
+
+                 netd =eta.plusHours(16).plusMinutes(45);
+             } else if (hora <= 780) {
+
+                 netd =eta.plusHours(16).plusMinutes(45);
+             } else if (hora <= 840) {
+
+                 netd =eta.plusHours(17).plusMinutes(45);
+             } else if (hora <= 900) {
+
+                 netd = eta.plusHours(17).plusMinutes(45);
+             } else if (hora <= 960) {
+
+                 netd = eta.plusHours(18).plusMinutes(45);
+             }
+             //Más de 50% del nocturno
+         } else {
+             if (hora <= 360) {
+
+                 netd = eta.plusHours(12).plusMinutes(45);
+             } else if (hora <= 480) {
+
+                 netd =  eta.plusHours(14).plusMinutes(45);
+             } else if (hora <= 540) {
+
+                 netd =  eta.plusHours(15).plusMinutes(45);
+             } else if (hora <= 600) {
+
+                 netd =  eta.plusHours(16).plusMinutes(45);
+             } else if (hora <= 660) {
+
+                 netd =  eta.plusHours(17).plusMinutes(45);
+             } else if (hora <= 720) {
+
+                 netd =  eta.plusHours(18).plusMinutes(45);
+             } else if (hora <= 780) {
+
+                 netd =  eta.plusHours(18).plusMinutes(45);
+             } else if (hora <= 840) {
+
+                 netd =   eta.plusHours(19).plusMinutes(45);
+             } else if (hora <= 900) {
+
+                 netd =  eta.plusHours(19).plusMinutes(45);
+             } else if (hora <= 960) {
+
+                 netd =  eta.plusHours(20).plusMinutes(45);
+             }
+         }
+            //Cumplido el descanso más 1 hora para nuevo ETD
+         nuevoEtd.setText("Próximo ETD: " + netd.plusHours(1));
+     }
+ }
+
+
+
 
 
 
--- app/src/main/java/com/example/calculadoravuelo/VencimientoCabActivity.java
+++ app/src/main/java/com/example/calculadoravuelo/VencimientoCabActivity.java
@@ -1,34 +1,25 @@
 package com.example.calculadoravuelo;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class VencimientoCabActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_vencimiento_cab);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.menu_principal, menu);
-
-        return true;
-    }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -62,6 +53,29 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+        int h1 = 0, h2 = 0, m1 = 0, m2 = 0;
+        int hora = 0, h = 0, m = 0;
+        TextView res, ha, hb, nuevoEtd;
 
 
+        public void primero(View v) {
+        //Inicio del tiempo de servicio de vuelo (1 hora antes del ETD)
+        final Calendar cal = Calendar.getInstance();
+        final int hora = cal.get(Calendar.HOUR_OF_DAY);
+        final int minuto = cal.get(Calendar.MINUTE);
+        TimePickerDialog recogerHora = new TimePickerDialog(this, (view, horaDia, minutos) -> {
+            h1 = horaDia;
+            m1 = minutos;
+            String horaF = (horaDia < 10) ? "0" + horaDia : String.valueOf(horaDia);
+            String minutoF = (minutos < 10) ? "0" + minutos : String.valueOf(minutos);
+            String AM_PM = (horaDia < 12) ? "a.m." : "p.m.";
+            Toast.makeText(getApplication(), horaF + ":" + minutoF + " " + AM_PM, Toast.LENGTH_LONG).show();
+            ha.setText(horaF + ":" + minutoF + AM_PM);
+            //diferencia();
+           // descanso();
+
+        }, hora, minuto, true);
+        recogerHora.show();
+
+    }
 }
